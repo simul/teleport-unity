b54cc0ff0e1494d391ad2b007ce108fb4f1b7c4a
Roderick Kennedy <roderick.kennedy@gmail.com>
2020-01-27 18:07:09 +0000

    Added TeleportDll.cs for imported C++ fns and DiscoveryService.cs for c# implementation of client discovery.

--- Scripts/TeleportDll.cs
+++ Scripts/TeleportDll.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+
 namespace teleport
 {
-    public class Teleport : MonoBehaviour
+    public class TeleportDll
     {
+        public const string teleport_server_dll = @"Teleport";
+        static bool _initialized = false;
+        [DllImport(teleport_server_dll)]
+        private static extern void RegisterPlugin();
 
         // Start is called before the first frame update
         void Start()
