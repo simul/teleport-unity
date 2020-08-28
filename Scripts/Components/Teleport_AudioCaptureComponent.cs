a2324fe9370251bdea606c9f599b0f4b99484b12
Aidan Clear <aidan@simul.co>
2020-08-28 17:46:12 +0100

    Updating settings to allow enabling/disabling audio

--- Scripts/Components/Teleport_AudioCaptureComponent.cs
+++ Scripts/Components/Teleport_AudioCaptureComponent.cs
@@ -13,65 +13,78 @@ namespace teleport
     {
         #region DLLImports
         [DllImport("SimulCasterServer")]
+        static extern void InitializeAudioEncoder(uid clientID, ref SCServer.AudioEncodeParams audioEncodeParams);
+        [DllImport("SimulCasterServer")]
         static extern void SendAudio(uid clientID, IntPtr data, UInt64 dataSize);
         #endregion
 
-        public double bpm = 140.0F;
-        public float gain = 0.5F;
-        public int signatureHi = 4;
-        public int signatureLo = 4;
+        public uid clientID = 0;
 
-        private double nextTick = 0.0F;
-        private float amp = 0.0F;
-        private float phase = 0.0F;
-        private double sampleRate = 0.0F;
-        private int accent;
-        private bool running = false;
+        TeleportSettings teleportSettings = null;
+        bool running = false;
 
         void Start()
         {
-            accent = signatureHi;
-            double startTick = AudioSettings.dspTime;
-            sampleRate = AudioSettings.outputSampleRate;
-            nextTick = startTick * sampleRate;
-            running = true;
+            teleportSettings = TeleportSettings.GetOrCreateSettings();
+            //double startTick = AudioSettings.dspTime;
+            // sampleRate = AudioSettings.outputSampleRate;
         }
 
+        private void OnEnable()
+        {
+
+        }
+
+        void OnDisable()
+        {
+            
+        }
+
+        void LateUpdate()
+        {
+            if (!teleportSettings.casterSettings.isStreamingAudio)
+            {
+                return;
+            }
+
+            // for now just get latest client
+            uid id = Teleport_SessionComponent.GetLastClientID();
+
+            if (id != clientID)
+            {
+                clientID = id;
+
+                if (clientID != 0)
+                {
+                    Initialize();
+                    running = true;
+                }
+                else
+                {
+                    running = false;
+                }
+            }
+        }
+
+        void Initialize()
+        {
+            var audioParams = new SCServer.AudioEncodeParams();
+            audioParams.codec = avs.AudioCodec.PCM;
+            InitializeAudioEncoder(clientID, ref audioParams);
+        }
+
+        // This function is called on the audio thread
         void OnAudioFilterRead(float[] data, int channels)
         {
-            if (!running)
+            if (!running || data.Length <= 0)
                 return;
 
-
-            //double samplesPerTick = sampleRate * 60.0F / bpm * 4.0F / signatureLo;
-            //double sample = AudioSettings.dspTime * sampleRate;
-            //int dataLen = data.Length / channels;
-
-            //int n = 0;
-            //while (n < dataLen)
-            //{
-            //    float x = gain * amp * Mathf.Sin(phase);
-            //    int i = 0;
-            //    while (i < channels)
-            //    {
-            //        data[n * channels + i] += x;
-            //        i++;
-            //    }
-            //    while (sample + n >= nextTick)
-            //    {
-            //        nextTick += samplesPerTick;
-            //        amp = 1.0F;
-            //        if (++accent > signatureHi)
-            //        {
-            //            accent = 1;
-            //            amp *= 2.0F;
-            //        }
-            //        Debug.Log("Tick: " + accent + "/" + signatureHi);
-            //    }
-            //    phase += amp * 0.3F;
-            //    amp *= 0.993F;
-            //    n++;
-            //}
+            var sizeInBytes = data.Length * 4;
+            IntPtr ptr = Marshal.AllocHGlobal(sizeInBytes);
+            Marshal.Copy(data, 0, ptr, data.Length);
+            // Send audio to the client
+            SendAudio(clientID, ptr, (UInt64)sizeInBytes);
+            Marshal.FreeHGlobal(ptr);
         }
     }
 }
