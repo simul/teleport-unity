e05dcb3ca8f91d40ca3e7e1bd3dae54c577b38a5
GMostyn-Parry <georgemostyn-parry@outlook.com>
2021-06-01 10:00:37 +0100

    Can now retrieve input axis value from Teleport_Controller.
    + Added LogWarningOnce(...) and LogOnce(...) to TeleportLog.
    # Moved static controllers dictionary initialisation to OnEnable(...) from Awake(...), so it will be filled after a hot-reload.

--- Scripts/Debug/TeleportLog.cs
+++ Scripts/Debug/TeleportLog.cs
@@ -6,6 +6,10 @@ namespace teleport
 {
 	public static class TeleportLog
 	{
+		//Contains the hashes of the log messages we have received.
+		private static HashSet<int> logHashes = new HashSet<int>();
+		//Contains the hashes of the warning messages we have received.
+		private static HashSet<int> warningHashes = new HashSet<int>();
 		//Contains the hashes of the error messages we have received.
 		private static HashSet<int> errorHashes = new HashSet<int>();
 
@@ -13,6 +17,32 @@ namespace teleport
 		public static void ClearHashes()
 		{
 			errorHashes.Clear();
+		}
+
+		//Prints a log message, if we have never printed the message before.
+		public static void LogOnce(string message)
+		{
+			int hash = message.GetHashCode();
+			if(logHashes.Contains(hash))
+			{
+				return;
+			}
+
+			Debug.Log(message);
+			logHashes.Add(hash);
+		}
+
+		//Prints a warning message, if we have never printed the message before.
+		public static void LogWarningOnce(string message)
+		{
+			int hash = message.GetHashCode();
+			if(warningHashes.Contains(hash))
+			{
+				return;
+			}
+
+			Debug.LogWarning(message);
+			warningHashes.Add(hash);
 		}
 
 		//Prints an error message, if we have never printed the message before.
