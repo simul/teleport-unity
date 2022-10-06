22966655a4666ec16c2506de5f5c1ea6613cec05
Andrew Richards <andyjrichards93@gmail.com>
2022-10-06 14:41:03 +0100

    Initial commit for main geometry in Demo scene.

---
+++ Scripts/RotateShipSection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateShipSection : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		transform.rotation = new Quaternion();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var ea = transform.rotation.eulerAngles;
+		ea.z++;
+	}
+}
