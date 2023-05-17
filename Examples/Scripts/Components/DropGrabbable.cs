6429429cab0f78558a552384e5bce7e552a0e4f7
Roderick Kennedy <roderick.kennedy@gmail.com>
2023-05-17 15:32:19 +0100

    Added example DropGrabbable.

---
+++ Examples/Scripts/Components/DropGrabbable.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using uid = System.UInt64;
+
+namespace teleport
+{
+	using InputID = System.UInt16;
+	/// <summary>
+	/// An example component to drop a grabbable object.
+	/// </summary>
+	public class DropGrabbable : MonoBehaviour
+	{
+	}
+}
