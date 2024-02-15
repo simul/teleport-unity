9eef91511c1a9fc98b88f482d0fd9b47fcd8b06c
Roderick Kennedy <roderick.kennedy@gmail.com>
2024-02-15 12:05:01 +0000

    Added Streamable/Link.cs

---
+++ Scripts/Components/Streamable/Link.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace teleport
+{
+	public class Link : MonoBehaviour
+    {
+        public string url;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
